8da3e0c3cea724e539182d16f375cf1b82ede46a
caliusantos7 <caliu700@hotmail.com>
2022-09-07 18:20:50 -0300

    Testando

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestandoC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            soma = a+b;
+            x= "1";
+            x= "3"; 
+
+            soma = a+b;
+            
+            Console.WriteLine("O resultado da soma é:");
+        }
+    }
+}
